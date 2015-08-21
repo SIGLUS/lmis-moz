0c210d64b2140395c1273422b3d2914b7fa9e42c
coderliux <ecliujie@gmail.com>
2015-08-21 15:25:52 +0800

    [ JL ] - fix the deploy package permission

--- ci/deploy-web-to-uat.sh
+++ ci/deploy-web-to-uat.sh
@@ -1,4 +1,4 @@
 sudo /etc/init.d/tomcat7 stop
-rm -rf /var/lib/tomcat7/webapps/ROOT
+sudo rm -rf /var/lib/tomcat7/webapps/ROOT
 mv /tmp/openlmis-web.war /var/lib/tomcat7/webapps/ROOT.war
 sudo /etc/init.d/tomcat7 restart
