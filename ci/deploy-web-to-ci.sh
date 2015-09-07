05982bb6dde6e6996baef6b3879599ddbf92415c
Danni Yu <dyu@thoughtworks.com>
2015-09-07 16:17:31 +0800

    correct typo

--- ci/deploy-web-to-ci.sh
+++ ci/deploy-web-to-ci.sh
@@ -1,5 +1,5 @@
 /usr/share/tomcat/bin/shutdown.sh
-rm /user/share/tomcat/webapps/ROOT.war
-rm -rf /user/share/tomcat/webapps/ROOT
+rm /usr/share/tomcat/webapps/ROOT.war
+rm -rf /usr/share/tomcat/webapps/ROOT
 cp /var/lib/jenkins/jobs/moz-build/lastSuccessful/archive/open-lmis/modules/openlmis-web/build/libs/openlmis-web.war /usr/share/tomcat/webapps/ROOT.war
 /usr/share/tomcat/bin/startup.sh
