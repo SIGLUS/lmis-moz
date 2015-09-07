80b8df715d44089d45bc6f7bb04a628b70b062c8
Danni Yu <dyu@thoughtworks.com>
2015-09-07 15:52:44 +0800

    correct deployment script

--- ci/deploy-web-to-ci.sh
+++ ci/deploy-web-to-ci.sh
@@ -1,5 +1,5 @@
 /usr/share/tomcat/bin/shutdown.sh
-rm /var/lib/tomcat/webapps/ROOT.war
-rm -rf /var/lib/tomcat/webapps/ROOT
-cp /var/lib/jenkins/jobs/moz-build/lastSuccessful/archive/open-lmis/modules/openlmis-web/build/libs/openlmis-web.war /var/lib/tomcat/webapps/ROOT.war
+rm /user/share/tomcat/webapps/ROOT.war
+rm -rf /user/share/tomcat/webapps/ROOT
+cp /var/lib/jenkins/jobs/moz-build/lastSuccessful/archive/open-lmis/modules/openlmis-web/build/libs/openlmis-web.war /usr/share/tomcat/webapps/ROOT.war
 /usr/share/tomcat/bin/startup.sh
