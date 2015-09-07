bfb80ad0a2d252d7954f7429dfbffd7e5e74cb93
Danni Yu <dyu@thoughtworks.com>
2015-09-07 15:15:34 +0800

    change jenkins ci deploy script

--- ci/deploy-web-to-ci.sh
+++ ci/deploy-web-to-ci.sh
@@ -1,5 +1,5 @@
 /usr/share/tomcat/bin/shutdown.sh
-rm -rf /var/lib/tomcat/webapps/ROOT
-mv /var/lib/tomcat/webapps/openlmis-web.war /var/lib/tomcat/webapps/ROOT.war
 rm /var/lib/tomcat/webapps/ROOT.war
+rm -rf /var/lib/tomcat/webapps/ROOT
+cp /var/lib/jenkins/jobs/moz-build/lastSuccessful/archive/open-lmis/modules/openlmis-web/build/libs/openlmis-web.war /var/lib/tomcat/webapps/ROOT.war
 /usr/share/tomcat/bin/startup.sh
