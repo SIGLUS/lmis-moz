9267044944661e0100bff24c774243ec337c662b
Danni Yu <dyu@thoughtworks.com>
2015-09-16 10:29:29 +0800

    add db migrations for length

--- data/sql/qa/seed.sql
+++ data/sql/qa/seed.sql
@@ -1,5 +1,7 @@
-\i V1__programs.sql
-\i V2__geographic_levels.sql
-\i V3__facility_types.sql
-\i V4__dosage_units.sql
-\i V5__product_forms.sql
+\i V1__alter_products_strength_length.sql
+\i V2__alter_product_forms_code_length.sql
+\i V3__programs.sql
+\i V4__geographic_levels.sql
+\i V5__facility_types.sql
+\i V6__dosage_units.sql
+\i V7__product_forms.sql
