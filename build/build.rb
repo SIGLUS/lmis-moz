7d46f591139859fb2ca8b48f7d751416ec539cb5
Danni Yu <dyu@thoughtworks.com>
2015-09-16 10:30:39 +0800

    dont replace db migrations

--- build/build.rb
+++ build/build.rb
@@ -26,8 +26,6 @@ def replace_file_list
    'modules/openlmis-web/src/main/resources/applicationContext.xml',
    'modules/openlmis-web/build.gradle',
    'modules/rest-api/build.gradle',
-   'modules/db/src/main/resources/db/migration/V5_2__create_product_forms.sql',
-   'modules/db/src/main/resources/db/migration/V5_5__create_products.sql',
    'settings.gradle']
 end
 
