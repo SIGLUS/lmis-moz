91481d56d093a40d3bd3795e03060a609774d5be
Danni Yu <dyu@thoughtworks.com>
2015-09-16 10:54:35 +0800

    fix seed

--- data/sql/qa/seed.sql
+++ data/sql/qa/seed.sql
@@ -1,7 +1,6 @@
 \i V1__alter_products_strength_length.sql
-\i V2__alter_product_forms_code_length.sql
-\i V3__programs.sql
-\i V4__geographic_levels.sql
-\i V5__facility_types.sql
-\i V6__dosage_units.sql
-\i V7__product_forms.sql
+\i V2__programs.sql
+\i V3__geographic_levels.sql
+\i V4__facility_types.sql
+\i V5__dosage_units.sql
+\i V6__product_forms.sql
