a1262787084efabcadb807b765f7255d8c76895f
Danni Yu <dyu@thoughtworks.com>
2016-01-04 16:40:24 +0800

    DY - #242 - change product code for products deactivated

--- data/functional_tests/sql/deactivate_products.sql
+++ data/functional_tests/sql/deactivate_products.sql
@@ -1,2 +1,2 @@
-UPDATE products set active=FALSE, modifieddate=now() WHERE code = '01A01';
-UPDATE products set active=FALSE, modifieddate=now() WHERE code = '01A02';
+UPDATE products set active=FALSE, modifieddate=now() WHERE code = '12D03';
+UPDATE products set active=FALSE, modifieddate=now() WHERE code = '07L01';
