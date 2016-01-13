298da5d43537adffddeb2431db8b115a3d6e2346
zhouyu11 <yuzhou@thoughtworks.com>
2016-01-13 22:12:11 +0800

    ZY-#000- rename file

--- data/functional_tests/deactivate_products_have_stock_movement.sh
+++ data/functional_tests/deactivate_products_have_stock_movement.sh
@@ -1,1 +1,1 @@
-psql -U postgres --file data/functional_tests/sql/deactivate_product_has_stock_movement.sql -w open_lmis -h localhost
+psql -U postgres --file data/functional_tests/sql/deactivate_product_have_stock_movement.sql -w open_lmis -h localhost
