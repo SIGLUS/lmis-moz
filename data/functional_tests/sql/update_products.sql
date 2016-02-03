a81fc423481f96cefd361d743aed8c80f7c89058
Yingli Yuan <ylyuan@thoughtworks.com>
2016-02-03 13:29:22 +0800

    YL - #000 - update script

--- data/functional_tests/sql/update_products.sql
+++ data/functional_tests/sql/update_products.sql
@@ -2,7 +2,7 @@ INSERT INTO products (code, primaryName, dispensingUnit, dosesPerDispensingUnit,
 ('99X99', 'New Drug', '1', 1,1,TRUE,TRUE,FALSE,0,FALSE, '0,25mg', (SELECT id from product_forms where code='Comprimidos'));
 
 INSERT INTO program_products (programId, productId, fullSupply, active, dosesPerMonth, productCategoryId, currentPrice, displayOrder) VALUES
-((SELECT id FROM programs WHERE code = 'ESS_MEDS'), (SELECT id FROM products WHERE code = '99X99'), TRUE, TRUE,12.5, (SELECT id FROM product_categories where code = 'C1'), 0.0,  14);
+((SELECT id FROM programs WHERE code = 'ESS_MEDS'), (SELECT id FROM products WHERE code = '99X99'), TRUE, TRUE,12.5, (SELECT id FROM product_categories where code = '1'), 0.0,  14);
 
 INSERT INTO facility_approved_products (facilityTypeId, programProductId, maxMonthsOfStock) VALUES
 ((SELECT id FROM facility_types WHERE code = 'CSRUR-II'),(SELECT id FROM program_products WHERE productId = (SELECT id FROM products WHERE code = '99X99')), 1);
