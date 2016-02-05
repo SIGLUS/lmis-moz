716cd7bd9ff297926739dfd226c4625f51db05c0
Danni Yu <dyu@thoughtworks.com>
2016-02-05 11:04:48 +0800

    DY - #567 - reset showcase data

--- data/setup/showcase/seed.sql
+++ data/setup/showcase/seed.sql
@@ -1,4 +1,4 @@
-\i cleandata.sql
-\i configurations.sql
-\i productinfo.sql
-\i userinfo.sql
+\i data/setup/showcase/cleandata.sql
+\i data/setup/showcase/configurations.sql
+\i data/setup/showcase/productinfo.sql
+\i data/setup/showcase/userinfo.sql
