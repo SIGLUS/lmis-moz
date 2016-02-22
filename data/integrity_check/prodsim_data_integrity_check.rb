704045a6d7d2bc9cbdd400b78e57e7e299171b8a
cuipengfei <cuipengfei2008@gmail.com>
2016-02-22 16:29:12 +0800

    CPF -608- output soh even when there is no mismatch

--- data/integrity_check/prodsim_data_integrity_check.rb
+++ data/integrity_check/prodsim_data_integrity_check.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 sqloutput=`psql -U openlmis --file data/integrity_check/check_prodsim_soh.sql -w open_lmis -h 52.193.130.59 -p 5432`
 if sqloutput.include? "0 rows"
-  puts "no soh mismatch found"
+  puts "no soh mismatch found, sql output is:"
+  puts sqloutput
 else
   puts "mismatches found, here they are: "
   puts sqloutput
