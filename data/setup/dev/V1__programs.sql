e6c0d97e7cb8ba06556826b6fa476467b597e6a6
Danni Yu <dyu@thoughtworks.com>
2016-03-18 01:15:19 +0800

    DY - #642 - add new programs

--- data/setup/dev/V1__programs.sql
+++ data/setup/dev/V1__programs.sql
@@ -1,10 +1,9 @@
 INSERT INTO programs (code, name, description, active, templateConfigured,
   regimenTemplateConfigured, budgetingApplies, usesDar, push) VALUES
 ('MMIA', 'MMIA', 'MMIA', TRUE, FALSE, TRUE, FALSE, FALSE, FALSE),
-('VIA', 'VIA', 'VIA', TRUE, FALSE, TRUE, FALSE, FALSE, FALSE),
 ('ESS_MEDS', 'ESSENTIAL MEDICINE', 'ESSENTIAL MEDICINE', TRUE, FALSE, FALSE, FALSE, FALSE, FALSE),
 ('TB', 'TB', 'TB', TRUE, FALSE, TRUE, FALSE, FALSE, FALSE),
 ('NUTRITION', 'NUTRITION', 'NUTRITION', TRUE, FALSE, TRUE, FALSE, FALSE, FALSE);
 
-UPDATE programs SET parentId = (SELECT id FROM programs WHERE code = 'VIA')
-WHERE code in ('ESS_MEDS', 'TB', 'NUTRITION');
+UPDATE programs SET parentId = (SELECT id FROM programs WHERE code = 'ESS_MEDS')
+WHERE code in ('TB', 'NUTRITION');
