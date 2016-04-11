60238279d3b0d74443e2df5fbe1df6891ed98fbd
sherlockHK <576717731@qq.com>
2016-04-11 15:53:26 +0800

    HK - #643 -add displayorder column when insert new program-product.

--- data/one_time_migrations/find_different_program_product.py
+++ data/one_time_migrations/find_different_program_product.py
@@ -21,7 +21,7 @@ def create_new_product_table_and_copy_from_new_product_csv_file(csv_path):
 
 def find_diff_between_new_and_old_program_products():
     cursor.execute(
-        "SELECT p.code,pr.code,new.program,pp.productid,pp.productcategoryid,pp.dosespermonth,pp.currentprice,pp.createdby,pp.modifiedby,pp.fullsupply,pp.id "
+        "SELECT p.code,pr.code,new.program,pp.displayorder,pp.productcategoryid,pp.dosespermonth,pp.currentprice,pp.createdby,pp.modifiedby,pp.fullsupply,pp.id "
         "FROM new_products as new ,program_products as pp inner JOIN programs as pr ON pr.id = pp.programid INNER JOIN products as p on pp.productid = p.id "
         "WHERE new.code = p.code AND new.program!= pr.code;")
     diff_program_products = cursor.fetchall()
@@ -29,7 +29,7 @@ def find_diff_between_new_and_old_program_products():
     for program_product in diff_program_products:
         product_code = program_product[0]
         new_program_code = program_product[2]
-        productid = program_product[3]
+        displayorder = program_product[3]
         productcategoryid = program_product[4]
         dosespermonth = program_product[5]
         currentprice = program_product[6]
@@ -45,9 +45,9 @@ def find_diff_between_new_and_old_program_products():
         cursor.execute(deactive)
         pprint.pprint(deactive)
 
-        insert = "INSERT INTO program_products(programid,productid,productcategoryid,dosespermonth,isacoefficientsid,active,currentprice,createdby,createddate,modifiedby,modifieddate,fullsupply) " \
-                 "VALUES ((SELECT id FROM programs WHERE code = '%s'),(SELECT p.id from products p WHERE p.code ='%s'),%s,%s,NULL,TRUE ,%s,%s,current_timestamp,%s,current_timestamp,%s);" \
-                 % (new_program_code, product_code, productcategoryid, dosespermonth, currentprice, createdby, modifiedby,
+        insert = "INSERT INTO program_products(programid,productid,productcategoryid,dosespermonth,isacoefficientsid,displayorder,active,currentprice,createdby,createddate,modifiedby,modifieddate,fullsupply) " \
+                 "VALUES ((SELECT id FROM programs WHERE code = '%s'),(SELECT p.id from products p WHERE p.code ='%s'),%s,%s,NULL,%s,TRUE ,%s,%s,current_timestamp,%s,current_timestamp,%s);" \
+                 % (new_program_code, product_code, productcategoryid, dosespermonth, displayorder,currentprice, createdby, modifiedby,
                     fullsupply)
         cursor.execute(insert)
         pprint.pprint(insert)
