43073e1e66d777a0de18929af75b4e7ee6bb82ce
Danni Yu <dyu@thoughtworks.com>
2016-06-29 11:42:31 +0800

    Revert "DY - temporarily get rid of this for testing merge"
    This reverts commit d82892dbf21ee393b28a36ee42297ac87dea2a57.

--- build/buildSteps.rb
+++ build/buildSteps.rb
@@ -24,7 +24,8 @@ end
 
 def replace_file_list
   ['modules/openlmis-web/src/main/resources/openlmis_logging.xml',
-   'modules/openlmis-web/build.gradle']
+   'modules/openlmis-web/build.gradle',
+   'modules/db/src/main/resources/db/migration/V5_2__create_product_forms.sql']
 end
 
 def properties_files
