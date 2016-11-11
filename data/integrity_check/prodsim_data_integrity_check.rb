da87f2d081d6b61ba425ccfcd5b22bdafb948021
wwdzhang <wwdzhang@thoughtworks.com>
2016-11-11 14:02:53 +0800

    make the ruby file include all the script output and check them

--- data/integrity_check/prodsim_data_integrity_check.rb
+++ data/integrity_check/prodsim_data_integrity_check.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 sqloutput=`psql -U openlmis --file data/integrity_check/check_prodsim_soh.sql -w open_lmis -h 52.193.130.59 -p 5432`
-if sqloutput.include? "0 rows"
+def count_em(string, substring)
+  string.scan(/(?=#{substring})/).count
+end
+if count_em(sqloutput,"0 rows")== 4
   puts "no soh mismatch found, sql output is:"
   puts sqloutput
 else
