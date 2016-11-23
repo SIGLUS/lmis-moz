cadc663885d42f9b6c082efcf6651ea45f8de7ab
Tianxi Ku <kutianxi@outlook.com>
2016-11-23 15:04:15 +0800

    TK - 996 - rename script to avoid confusion

--- data/functional_tests/regression/reactivate_products_have_stock_movement.sh
+++ data/functional_tests/regression/reactivate_products_have_stock_movement.sh
@@ -1,1 +1,1 @@
-psql -U postgres --file data/functional_tests/sql/reactivate_products.sql -w open_lmis -h 52.193.34.16
+psql -U postgres --file data/functional_tests/sql/reactivate_products_have_stock_movement.sql -w open_lmis -h 52.193.34.16
