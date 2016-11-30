1f111e373a0ca89d129a89c5ab6fd0c385a8147f
Danni Yu <dyu@thoughtworks.com>
2016-11-30 11:40:01 +0800

    DY - #000 update clean data script

--- data/one_time_migrations/clean_up_prod_test_data.sql
+++ data/one_time_migrations/clean_up_prod_test_data.sql
@@ -1,24 +1,27 @@
-DELETE FROM moz_app_info WHERE facilityid IN (1, 13, 14);
+DELETE FROM moz_app_info WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47);
 
-DELETE FROM stock_card_entry_key_values WHERE stockcardentryid IN (SELECT id FROM stock_card_entries WHERE createdby IN (2, 4, 5, 6, 7, 8, 13, 14, 15, 33, 34, 35, 36, 37, 49));
-DELETE FROM stock_card_entries WHERE createdby IN (2, 4, 5, 6, 7, 8, 13, 14, 15, 33, 34, 35, 36, 37, 49);
-DELETE FROM stock_cards WHERE facilityid IN (1, 13, 14);
+DELETE FROM stock_card_entry_lot_items_key_values WHERE stockcardentrylotitemid IN (SELECT id FROM stock_card_entry_lot_items WHERE stockcardentryid IN (SELECT id FROM stock_card_entries WHERE stockcardid IN (SELECT id FROM stock_cards WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47))));
+DELETE FROM stock_card_entry_lot_items WHERE stockcardentryid IN (SELECT id FROM stock_card_entries WHERE stockcardid IN (SELECT id FROM stock_cards WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47)));
+DELETE FROM stock_card_entry_key_values WHERE stockcardentryid IN (SELECT id FROM stock_card_entries WHERE stockcardid IN (SELECT id FROM stock_cards WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47)));
+DELETE FROM stock_card_entries WHERE stockcardid IN (SELECT id FROM stock_cards WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47));
+DELETE FROM lots_on_hand WHERE stockcardid IN (SELECT id FROM stock_cards WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47));
+DELETE FROM stock_cards WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47);
 
-DELETE FROM requisition_signatures WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (1, 13, 14));
-DELETE FROM signatures WHERE id IN (3, 4, 13 ,14, 15, 16, 17, 18, 29, 30, 31, 32, 33, 34, 39, 40, 41, 42, 51, 52, 53, 54);
+DELETE FROM requisition_signatures WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47));
+-- DELETE FROM signatures WHERE id IN (3, 4, 13 ,14, 15, 16, 17, 18, 29, 30, 31, 32, 33, 34, 39, 40, 41, 42, 51, 52, 53, 54);
 
-DELETE FROM patient_quantification_line_items WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (1, 13, 14));
+DELETE FROM patient_quantification_line_items WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47));
 
-DELETE FROM regimen_line_items WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (1, 13, 14));
+DELETE FROM regimen_line_items WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47));
 
-DELETE FROM requisition_line_items WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (1, 13, 14));
-DELETE FROM requisition_status_changes WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (1, 13, 14));
-DELETE FROM requisition_periods WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (1, 13, 14));
-DELETE FROM requisitions WHERE facilityid IN (1, 13, 14);
+DELETE FROM requisition_line_items WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47));
+DELETE FROM requisition_status_changes WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47));
+DELETE FROM requisition_periods WHERE rnrid IN (SELECT id FROM requisitions WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47));
+DELETE FROM requisitions WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47);
 
-DELETE FROM archived_products WHERE facilityid IN (2, 6, 8, 14, 16, 17, 18, 19)
+DELETE FROM archived_products WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47)
 
-DELETE FROM requisition_group_members WHERE facilityid IN (1, 13, 14);
+DELETE FROM requisition_group_members WHERE facilityid IN (2, 3, 5, 6, 7, 8, 9, 10, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47);
 
 DELETE FROM user_password_reset_tokens WHERE userid IN (2, 4, 5, 6, 7, 8, 13, 14, 15, 33, 34, 35, 36, 37, 49);
 DELETE FROM role_assignments WHERE userid = (SELECT id FROM users WHERE username = 'chai_testuser');
